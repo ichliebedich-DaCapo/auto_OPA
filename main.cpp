b930797ab5dac721a603f4081f62b3d1c3ea615c
fairy <3475397231@qq.com>
2025-03-09 18:01:06 +0800

    【新法】可以搜索到方法，也有进度条，不过区间会封闭。并且结果太多，打印不完全

--- main.cpp
+++ main.cpp
@@ -1,191 +1,171 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 #include <thread>
 #include <mutex>
 #include <atomic>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
+#include <sstream>
 #include <getopt.h>
 
 using namespace std;
-// ANSI颜色代码
-#define COLOR_RESET   "\033[0m"
-#define COLOR_CYAN    "\033[36m"
-#define COLOR_GREEN   "\033[32m"
-#define COLOR_YELLOW  "\033[33m"
 
-struct Params
-{
-    double x1{}, x2{};
-    double v1{}, v2{};
-    double step{};
-    int threads{};
-    bool valid = false;
-};
-
-struct Result
-{
-    double O1min, O1max;
-    double O2min, O2max;
-    vector<pair<double, double> > intervals;
+struct Result {
+    double O1a, O1b, O2a, O2b;
+    double s1, s2, s3;
 };
 
 vector<Result> results;
 mutex results_mutex;
-atomic<long long> progress(0);
-long long total_combinations = 0;
 
-Params parse_args(int argc, char *argv[])
-{
-    Params p;
-    int opt;
+void worker(const vector<pair<double, double>>& O1_pairs,
+            const vector<pair<double, double>>& O2_pairs,
+            double x1, double x2, double vmin, double vmax,
+            atomic<size_t>& processed_pairs) {
+    for (const auto& o1_pair : O1_pairs) {
+        double O1a = o1_pair.first;
+        double O1b = o1_pair.second;
+        for (const auto& o2_pair : O2_pairs) {
+            double O2a = o2_pair.first;
+            double O2b = o2_pair.second;
+
+            double a = O1a * O2a;
+            double b = O1a * O2b;
+            double c = O1b * O2a;
+            double d = O1b * O2b;
+            vector<double> ks = {a, b, c, d};
+            sort(ks.rbegin(), ks.rend());
+            double k1 = ks[0], k2 = ks[1], k3 = ks[2], k4 = ks[3];
+
+            if (k1 > vmax / x1) continue;
+            if (k4 < vmin / x2) continue;
+
+            double s1 = max(x1, vmin / k2);
+            if (s1 > vmax / k1) continue;
+
+            double s2 = max(s1, vmin / k3);
+            if (s2 > vmax / k2) continue;
+
+            double s3 = max(s2, vmin / k4);
+            if (s3 > vmax / k3 || s3 > x2) continue;
+
+            if (!(s1 < s2 && s2 < s3)) continue;
+
+            lock_guard<mutex> lock(results_mutex);
+            results.push_back({O1a, O1b, O2a, O2b, s1, s2, s3});
+        }
+        processed_pairs++;
+    }
+}
+
+void print_progress(size_t processed, size_t total, size_t found) {
+    const int bar_width = 50;
+    float progress = static_cast<float>(processed) / total;
+    int pos = bar_width * progress;
+    string bar;
+    for (int i = 0; i < bar_width; ++i) {
+        if (i < pos) bar += "=";
+        else if (i == pos) bar += ">";
+        else bar += " ";
+    }
+    cout << "\033[34m\rProcessing: [" << bar << "] "
+         << fixed << setprecision(1) << (progress * 100.0) << "%"
+         << " Found: " << found << "\033[0m" << flush;
+}
+
+int main(int argc, char* argv[]) {
+    double x1 = 30, x2 = 600, vmin = 1000, vmax = 1900, step = 0.5;
+    int threads = 16;
+
+    // 解析命令行参数
+    // int opt;
     // while ((opt = getopt(argc, argv, "i:o:s:j:h")) != -1) {
     //     switch (opt) {
     //         case 'i':
-    //             p.x1 = stod(optarg);
-    //             p.x2 = stod(argv[optind++]);
+    //             x1 = stod(optarg);
+    //             x2 = stod(argv[optind++]);
     //             break;
     //         case 'o':
-    //             p.v1 = stod(optarg);
-    //             p.v2 = stod(argv[optind++]);
+    //             vmin = stod(optarg);
+    //             vmax = stod(argv[optind++]);
     //             break;
     //         case 's':
-    //             p.step = stod(optarg);
+    //             step = stod(optarg);
     //             break;
     //         case 'j':
-    //             p.threads = stoi(optarg);
+    //             threads = stoi(optarg);
     //             break;
     //         case 'h':
+    //             cout << "Usage: " << argv[0] << " -i x1 x2 -o vmin vmax -s step -j threads\n";
+    //             return 0;
     //         default:
-    //             cerr << "Usage: " << argv[0]
-    //                  << " -i x1 x2 -o v1 v2 -s step -j threads\n";
-    //             exit(1);
+    //             cerr << "Invalid arguments\n";
+    //             return 1;
     //     }
     // }
-    // if (p.x2 <= p.x1 || p.v2 <= p.v1 || p.step <= 0 || p.threads <= 0) {
-    //     cerr << "Invalid parameters\n";
-    //     exit(1);
-    // }
-    p.x1 = 30;
-    p.x2 = 600;
-    p.v1 = 500;
-    p.v2 = 1900;
-    p.step = 0.2;
-    p.threads = 16;
-    p.valid = true;
-    return p;
-}
 
-void calculate_range(const Params &p,
-                     double o1min_start, double o1min_end,
-                     double o2min_start, double o2min_end)
-{
-    for (double O1min = o1min_start; O1min <= o1min_end; O1min += p.step)
-    {
-        for (double O1max = O1min + p.step; O1max <= p.v2 / p.x1; O1max += p.step)
-        {
-            for (double O2min = o2min_start; O2min <= o2min_end; O2min += p.step)
-            {
-                for (double O2max = O2min + p.step; O2max <= p.v2 / p.x1; O2max += p.step)
-                {
-                    double gains[4] = {
-                        O1min * O2min,
-                        O1min * O2max,
-                        O1max * O2min,
-                        O1max * O2max
-                    };
-                    sort(gains, gains + 4, greater<double>());
+    // 生成O1和O2的可能值
+    vector<double> O1_values, O2_values;
+    for (double o = step; o <= vmax / x1; o += step) {
+        O1_values.push_back(o);
+        O2_values.push_back(o);
+    }
 
-                    double L[4], R[4];
-                    for (int i = 0; i < 4; ++i)
-                    {
-                        L[i] = p.v1 / gains[i];
-                        R[i] = p.v2 / gains[i];
-                    }
-
-                    vector<pair<double, double> > intervals;
-                    double prev_end = p.x1;
-                    bool valid = true;
-                    for (int i = 0; i < 4; ++i)
-                    {
-                        double start = max(prev_end, L[i]);
-                        double end = (i == 3) ? p.x2 : R[i];
-                        if (start > end)
-                        {
-                            valid = false;
-                            break;
-                        }
-                        if (end > p.x2) end = p.x2;
-                        intervals.emplace_back(start, end);
-                        prev_end = end;
-                    }
-
-                    if (valid && prev_end >= p.x2 - 1e-9)
-                    {
-                        Result res{O1min, O1max, O2min, O2max, intervals};
-                        lock_guard<mutex> lock(results_mutex);
-                        results.push_back(res);
-                    }
-                    ++progress;
-                }
-            }
+    // 生成O1和O2的两两组合
+    vector<pair<double, double>> O1_pairs, O2_pairs;
+    for (size_t i = 0; i < O1_values.size(); ++i) {
+        for (size_t j = i + 1; j < O1_values.size(); ++j) {
+            O1_pairs.emplace_back(O1_values[i], O1_values[j]);
         }
     }
-}
+    for (size_t i = 0; i < O2_values.size(); ++i) {
+        for (size_t j = i + 1; j < O2_values.size(); ++j) {
+            O2_pairs.emplace_back(O2_values[i], O2_values[j]);
+        }
+    }
 
-void show_progress(const Params &p)
-{
-    while (progress < total_combinations)
-    {
-        double percent = 100.0 * progress / total_combinations;
-        cout << "\033[34mProgress: [";
-        int pos = 50 * progress / total_combinations;
-        for (int i = 0; i < 50; ++i)
-            cout << (i < pos ? '=' : (i == pos ? '>' : ' '));
-        cout << "] " << fixed << setprecision(1) << percent << "%\r";
-        cout.flush();
+    size_t total_pairs = O1_pairs.size();
+    atomic<size_t> processed_pairs(0);
+
+    // 分配任务给线程
+    vector<thread> workers;
+    size_t chunk_size = (O1_pairs.size() + threads - 1) / threads;
+
+    for (int i = 0; i < threads; ++i) {
+        size_t start = i * chunk_size;
+        size_t end = min(start + chunk_size, O1_pairs.size());
+        vector<pair<double, double>> chunk(O1_pairs.begin() + start, O1_pairs.begin() + end);
+        workers.emplace_back(worker, chunk, O2_pairs, x1, x2, vmin, vmax, ref(processed_pairs));
+    }
+
+    // 显示进度条
+    while (processed_pairs < total_pairs) {
+        size_t current = processed_pairs.load();
+        size_t found = results.size();
+        print_progress(current, total_pairs, found);
         this_thread::sleep_for(chrono::milliseconds(100));
     }
-}
 
-int main(int argc, char *argv[])
-{
-    Params p = parse_args(argc, argv);
-    if (!p.valid) return 1;
-
-    vector<thread> threads;
-    int num_per_thread = ceil((p.v2 / p.x1) / p.step / p.threads);
-
-    for (int i = 0; i < p.threads; ++i)
-    {
-        threads.emplace_back([&p, i, num_per_thread]
-        {
-            const double o1min_start = i * num_per_thread * p.step;
-            const double o1min_end = min(o1min_start + num_per_thread * p.step, p.v2 / p.x1);
-            calculate_range(p, o1min_start, o1min_end, 0, p.v2 / p.x1);
-        });
+    for (auto& t : workers) {
+        t.join();
     }
+    print_progress(total_pairs, total_pairs, results.size());
+    cout << endl;
 
-    thread progress_thread(show_progress, cref(p));
-
-    for (auto &t: threads) t.join();
-    progress_thread.join();
-
-    cout << "\nFound " << results.size() << " results:\n";
-    for (size_t i = 0; i < results.size(); ++i)
-    {
+    // 输出结果
+    cout << "Found " << results.size() << " results:\n";
+    for (size_t i = 0; i < results.size(); ++i) {
+        const auto& r = results[i];
         cout << "Result " << i + 1 << ":\n";
-        printf("O1: [%.4f, %.4f]  O2: [%.4f, %.4f]\n",
-               results[i].O1min, results[i].O1max,
-               results[i].O2min, results[i].O2max);
-        cout << "Intervals:\n";
-        for (const auto &[fst, snd]: results[i].intervals)
-        {
-            printf("[%.6f, %.6f]\n", fst, snd);
-        }
-        cout << endl;
+        cout << "O1: [" << r.O1a << ", " << r.O1b << "]\n";
+        cout << "O2: [" << r.O2a << ", " << r.O2b << "]\n";
+        cout << "Segments: [" << x1 << ", " << r.s1 << "), ["
+             << r.s1 << ", " << r.s2 << "), ["
+             << r.s2 << ", " << r.s3 << "), ["
+             << r.s3 << ", " << x2 << "]\n\n";
     }
+
+    return 0;
 }
